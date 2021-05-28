cba46e2754eba7b70598a52d6730c9467f185eb1
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-05-28 20:25:18 +0530

    Global state, CSRF, axios interceptors and protected routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,16 @@
 import express from "express";
 
-import { register, login } from "../controllers/auth";
+// middleware
+import { requireSignin } from "../middlewares";
+
+// controllers
+import { register, login, logout, currentUser } from "../controllers/auth";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", logout);
+router.get("/current-user", requireSignin, currentUser);
 
 module.exports = router;
