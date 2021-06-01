a00dc49be396cb3aa75b2dcad380b73f36a87f7e
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-06-01 10:23:54 +0530

    Current instructor and protected routes

--- controllers/instructor.js
+++ controllers/instructor.js
@@ -36,21 +36,34 @@ export const getAccountStatus = async (req, res) => {
   try {
     const user = await User.findById(req.user._id).exec();
     const account = await stripe.accounts.retrieve(user.stripe_account_id);
-    console.log("ACCOUNT => ", account);
+    // console.log("ACCOUNT => ", account);
     if (!account.charges_enabled) {
-      return res.status(401).send("Unauthorized");
+      return res.staus(401).send("Unauthorized");
     } else {
       const statusUpdated = await User.findByIdAndUpdate(
         user._id,
         {
           stripe_seller: account,
-          $addToSet: { role: "Instructor" }, //addtoset make sure no duplicates
+          $addToSet: { role: "Instructor" },
         },
         { new: true }
       )
         .select("-password")
         .exec();
       res.json(statusUpdated);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const currentInstructor = async (req, res) => {
+  try {
+    let user = await User.findById(req.user._id).select("-password").exec();
+    if (!user.role.includes("Instructor")) {
+      return res.sendStatus(403);
+    } else {
+      res.json({ ok: true });
     }
   } catch (err) {
     console.log(err);
